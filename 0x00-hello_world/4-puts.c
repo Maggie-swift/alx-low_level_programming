0c6a8c55f0a909c497ac05a79122194b02623591
Maggie-swift <buxomy8@gmail.com>
2023-02-17 06:23:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,9 @@
 
 /** 
  * main - Entry point
- * description - Programming
+ * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
