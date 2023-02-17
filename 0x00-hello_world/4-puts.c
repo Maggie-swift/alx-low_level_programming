16108efa15dea1753f0e37684322d25290a8cb05
Maggie-swift <buxomy8@gmail.com>
2023-02-17 01:50:15 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
- /*
- * main -print a line of code using puts 
- *
+/*
+ * main - print a line of code using puts 
+ * description - {_programming in C_}  
  * Return: 0
- */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
