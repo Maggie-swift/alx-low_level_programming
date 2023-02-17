446609ac990d2af2950bfdea8ff9c0da996f46ea
Maggie-swift <buxomy8@gmail.com>
2023-02-17 02:55:41 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * Return : 0
  */
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
