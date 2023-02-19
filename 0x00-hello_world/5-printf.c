05a6661a4c6f5b9843df39fd54a236f7200d701b
Maggie-swift <buxomy8@gmail.com>
2023-02-19 02:34:49 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,11 @@
  *
  *description - use function provided
  *
- *Return - Always (0) 
+ *Return - Always (0)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
