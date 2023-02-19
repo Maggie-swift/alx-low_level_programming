d8330a9624e206a554137636767cbbd5ff449d02
Maggie-swift <buxomy8@gmail.com>
2023-02-19 09:07:33 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-	printf("Size of a char: %i byte(s)\n", sizeof (char));
+	printf("Size of a char: %d byte(s)\n", sizeof (char));
         printf("Size if a int: %d bytes(s)\n", sizoeof(int));	
 	printf("Size of a long int: %d byte(s)\n",sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n",sizeof(long long int));
