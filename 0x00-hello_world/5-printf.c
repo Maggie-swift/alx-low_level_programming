3f8e2254f5725707d1ec5abb0e2fd04efa62a69d
Maggie-swift <buxomy8@gmail.com>
2023-02-19 02:54:48 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,5 +8,8 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+/* 
+ * The main function returns 0 to indicate successful program execution. 
+ */
+return 0;
 }
