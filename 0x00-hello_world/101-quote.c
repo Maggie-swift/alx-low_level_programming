a40929ff4206e675f9150f9b3a9b7a73418f335d
Maggie-swift <buxomy8@gmail.com>
2023-02-19 05:43:00 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /*
@@ -9,6 +8,6 @@
 int main(void)
 
 {
-	putf("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",59)
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
