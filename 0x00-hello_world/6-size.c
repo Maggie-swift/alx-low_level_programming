4c294273a3c7cc313f3187961a9e063559d6555a
Maggie-swift <buxomy8@gmail.com>
2023-02-19 03:41:51 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*main - Entry
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", size of( char));
+	Printf(“Size of a int: %lu byte(s)\n", size of(int));
+	printf(“Size of a long int: %lu byte(s)\n", size of(long int));
+	printf(“Size of a long long: %lu byte(s)\n",size of(long long int));
+	printf(“Size of a float: %lu byte(s)\n", size of( float));
+}
