4ee996c205cbe81674b418e3053a57b0285f83d0
Maggie-swift <buxomy8@gmail.com>
2023-02-21 14:22:55 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,15 +4,28 @@
 /**
 *print_to_98 - prints all natural numbers from n to 98,
 *followed by a new line
-*@n: print from this number
+* @n: print from this number
 */
 
 void print_to_98(int n)
 {
-	print_to_98(0);
-        print_to_98(98);
-        print_to_98(111);
-        print_to_98(81);
-        print_to_98(-10);
-        return (0);
+	int i, j;
+
+	if (n <= 98)
+	{
+	for (i = n; i <= 98; i++)
+	{
+	if (i != 98
+	printf("%d, ", i);
+	else if (i == 98)
+	}
+	printf("%d\n", i);
+	}else if (n >= 98)
+	{
+	for (j = n; j >= 98; j--)
+	{
+	if (j != 98)
+        printf("%d, ", j);
+        else if (j == 98)
+	printf("%d\n", j);
 }
