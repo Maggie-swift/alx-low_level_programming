19c7368d2b699ea458b68a9c3701d1b3dd41101d
Maggie-swift <buxomy8@gmail.com>
2023-02-22 00:21:41 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,14 +7,12 @@
 
 int print_last_digit(int n)
 {
-
-	int a;	
+        int a
+	int a = n % 10;	
 
 	if (n < 0)
 	n = -n;
-        a = n % 10;	
-	if (a < 0)
-	a = -a;
+
 	_putchar(a + '0');
 	return (a);
 }
