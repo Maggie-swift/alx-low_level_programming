6d6a6182f200800fe789113cbce20fbf5049d8ee
Maggie-swift <buxomy8@gmail.com>
2023-02-22 02:36:42 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 100 Fibonacci numbers
- *
- * Return: Always 0 (Success)
+ * main - print the first 98 fibonacci number
+ * Return: void
  */
-
 int main(void)
 {
-	int i;
-	unsigned long int fib1 = 1;
-	unsigned long int fib2 = 2;
-	unsigned long int next;
+	unsigned int i;
+	unsigned long k, a, b, a2, b2;
 
-	printf("%lu, %lu", fib1, fib2);
-
-	for (i = 2; i < 100; i++)
+	a = 1, b = 2;
+	printf("%lu", a);
+	for (i = 0; i < 90; i++)
 	{
-		next = fib1 + fib2;
-		printf(", %lu", next);
-		fib1 = fib2;
-		fib2 = next;
+		printf(", %lu", b);
+		b += a, a = b - a;
+	}
+
+
+	/* use k to separate the large number into 2 halfs */
+	k = 1000000000;
+
+	/*
+	 * separate big number into 2 variables
+	 * x : first half
+	 * x2: seconnd half
+	 */
+	a2 = a % k, a /= k;
+	b2 = b % k, b /= k;
+
+	/* calculate fibonacci for large numbers */
+	for (i = 0; i < 7; i++)
+	{
+		printf(", %lu%lu", b + (b2 / k), b2 % k);
+		b += a, a = b - a;
+		b2 += a2, a2 = b2 - a2;
 	}
 
 	printf("\n");
-
 	return (0);
 }
