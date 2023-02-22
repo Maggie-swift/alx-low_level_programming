fca918bd0a3bca4dfffb7c382da0416391450165
Maggie-swift <buxomy8@gmail.com>
2023-02-22 01:45:39 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,7 @@
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
-void print_times_table(int n)
+void print times_table(int n)
 {
 	int i, j, k;
 
