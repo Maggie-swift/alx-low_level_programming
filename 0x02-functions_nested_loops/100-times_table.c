6651271fcb7e0216e305d5d17af331f8884087a6
Maggie-swift <buxomy8@gmail.com>
2023-02-22 03:54:57 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,61 +1,49 @@
 #include "main.h"
-
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
+ *
+ * Return: Nothing
  */
 void print_times_table(int n)
 {
-    int i, j, k;
+	int num1 = 0, num2, mult, ones, tenth, hundred;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n >= 0 && n <= 15)
+	{
+		while (num1 <= n)
+		{
+			num2 = 0;
 
-    for (i = 0; i <= n; i++)
-    {
-        _putchar('0');
-        for (j = 1; j <= n; j++)
-        {
-            k = i * j;
-            _putchar(',');
-            _putchar(' ');
-
-            if (k < 10)
-            {
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(k + '0');
-            }
-            else if (k < 100)
-            {
-                _putchar(' ');
-                _putchar(k / 10 + '0');
-                _putchar(k % 10 + '0');
-            }
-            else
-            {
-                _putchar(k / 100 + '0');
-                _putchar(k / 10 % 10 + '0');
-                _putchar(k % 10 + '0');
-            }
-        }
-        _putchar('\n');
-    }
+			while (num2 <= n)
+			{
+				mult = num1 * num2;
+				ones = mult % 10;
+				tenth = mult % 100 / 10;
+				hundred = mult / 100;
+				if (hundred == 0 && num2 != 0)
+				{
+					_putchar(' ');
+					if (tenth == 0)
+						_putchar(' ');
+					else
+						_putchar(tenth + '0');
+				}
+				else if (hundred != 0)
+				{
+					_putchar(hundred + '0');
+					_putchar(tenth + '0');
+				}
+				_putchar(ones + '0');
+				if (num2 != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				num2++;
+			}
+			_putchar('\n');
+			num1++;
+		}
+	}
 }
-
-int main(void)
-{
-    int tables[] = {2, 10, 13, 2, 0, 100};
-    int num_tables = sizeof(tables) / sizeof(tables[0]);
-    int i;
-
-    for (i = 0; i < num_tables; i++)
-    {
-        printf("Table of %d:\n", tables[i]);
-        print_times_table(tables[i]);
-        printf("\n");
-    }
-
-    return (0);
-}
-
