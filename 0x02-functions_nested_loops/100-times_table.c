d6891c4868f13463a09064a34429b0ea4973d828
Maggie-swift <buxomy8@gmail.com>
2023-02-22 01:30:28 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,30 +1,47 @@
-/**
-*File: 101-natural.c
-*Auth: Maggie-swift
-*/
-
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - Lists all the natural numbers below 1024 (excluded)
-*       that are multiples of 3 or 5.
-*
-*Return: Always 0.
-*/
-
-int main(void)
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
+void print_times_table(int n)
 {
-	i
-        int i, sum = 0;
-        
-        for (i = 0; i < 1024; i++)
-        {
-	       i	
-               if ((i % 3) == 0 || (i % 5) == 0)
-               sum += i;
-        }
+	int i, j, k;
 
-        printf("%d\n", sum);
-
-        return (0);
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
