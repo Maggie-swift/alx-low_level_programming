b4e1f736d30e5005f96ee7de9699b3e017a19050
Maggie-swift <buxomy8@gmail.com>
2023-02-21 22:06:56 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,13 +13,12 @@ void print_alphabet_x10(void)
 	i = (0);
         while (i < 10)
 	{
-		ch = ('a');
+		ch = 'a';
 		while (ch <= 'z')
 		{
 		       _putchar(ch);
 		       ch++;
 		}
-		{
 		       _putchar('\n');
 	               i++;
 	 }	
