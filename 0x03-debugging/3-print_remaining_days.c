a23817f0ff1b94b0e04eb08bebc79bfb45944e34
Maggie-swift <buxomy8@gmail.com>
2023-02-23 11:42:39 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -7,9 +7,9 @@
  * @month: month in number format
  * @day: day of month
  * @year: year
- *
  * Return: void
  */
+
 void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
