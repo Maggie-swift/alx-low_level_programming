c4e5ee63d2ccf21a57f4777cc483572db939650f
Maggie-swift <buxomy8@gmail.com>
2023-02-23 11:13:44 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,39 +3,34 @@
 
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
- *        but for multiples of three prints Fizz instead of the number
- *        and for the multiples of five prints Buzz
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 != 0)
-        {
-            printf(" Fizz");
-        }
-        else if (i % 5 == 0 && i % 3 != 0)
-        {
-            printf(" Buzz");
-        }
-        else if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf(" FizzBuzz");
-        }
-        else if (i == 1)
-        {
-            printf("%d", i);
-        }
-        else
-        {
-            printf(" %d", i);
-        }
-    }
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
 
-    printf("\n");
-
-    return (0);
+	return (0);
 }
