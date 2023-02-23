af00443e7dcd373bc9f5b78b6c28a860d1fab1ca
Maggie-swift <buxomy8@gmail.com>
2023-02-23 05:31:05 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
  */
 void print_numbers(void)
 {
-	int c;
-	for c = (48;c < 58, c++),
-	{
-        _putchar (c);
-	}
-	_putchar ('\n')
-	
+ int c;
+
+ for (c = 48; c < 58; c++)
+ {
+ _putchar(c);
+ }
+ _putchar('\n');
 }
