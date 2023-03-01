86bce770eb1f648dad00479ae298fec5f294af06
Maggie-swift <buxomy8@gmail.com>
2023-03-01 11:25:42 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,29 @@
-#include "main.h"                                                                                                                  
-#include <unistd.h>                                                                                                                     
-                                                                                                                                        
-/**                                                                                                                                     
- * _putchar - writes the character c to stdout                                                                                          
- * @c: The character to print                                                                                                           
- *                                                                                                                                      
- * Return: On success 1.                                                                                                                
- * On error, -1 is returned, and errno is set appropriately.                                                                            
- */                                                                                                                                     
-int _putchar(char c)                                                                                                                    
-{                                                                                                                                       
-        return (write(1, &c, 1));                                                                                                       
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
