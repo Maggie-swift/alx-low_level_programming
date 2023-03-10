d82cb7b2a3e121c10d7ea381fc32c78ee10f2d94
Maggie-swift <buxomy8@gmail.com>
2023-03-10 07:17:57 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,35 +1,16 @@
+#include "holberton.h"
 #include <stdio.h>
-		
-#include "main.h"
-		
 
-		
 /**
-		
- * main - print the number of arguments passed to the program
-		
- * @argc: number of arguments
-		
- * @argv: array of arguments
-		
- *
-		
- * Return: Always 0 (Success)
-		
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
  */
-		
 
-		
 int main(int argc, char *argv[])
-		
 {
-		
-(void) argv; /*Ignore argv*/
-		
-	printf("%d\n", argc - 1);
-		
+	printf("%s\n", argv[argc * 0]);
 
-		
 	return (0);
-		
 }
