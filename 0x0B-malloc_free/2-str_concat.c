49b646deddc20e485160a56019faf676805f056f
Maggie-swift <buxomy8@gmail.com>
2023-03-14 09:37:54 -0700

    0-create_array.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,57 +1,49 @@
-#include <stdio.h>
-		
 #include <stdlib.h>
-		
 #include "main.h"
-		
+
 /**
-		
- * _strdup - duplicate to new memory space location
-		
- * @str: char
-		
- * Return: 0
-		
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
+ *
+ * Return: pointer to the new string created (Success), or NULL (Error)
  */
-		
-char *_strdup(char *str)
-		
+char *str_concat(char *s1, char *s2)
 {
-		
-	char *aaa;
-		
-	int i, r = 0;
-		
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-		
-	if (str == NULL)
-		
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
-		
+
 	i = 0;
-		
-	while (str[i] != '\0')
-		
-		i++;
-		
+	j = 0;
 
-		
-	aaa = malloc(sizeof(char) * (i + 1));
-		
+	if (s1)
+	{
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
+	}
 
-		
-	if (aaa == NULL)
-		
-		return (NULL);
-		
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
 
-		
-	for (r = 0; str[r]; r++)
-		
-		aaa[r] = str[r];
-		
-
-		
-	return (aaa);
-		
+	return (s3);
 }
