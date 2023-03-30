b48484bb970932c7650b1e67e1e5853b29027f81
Maggie-swift <buxomy8@gmail.com>
2023-03-30 00:43:14 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,8 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 
     new->str = strdup(str);
 
-    for (nchar = 0; str[nchar]; nchar++)
-        ;
+    for (nchar = 0; str[nchar]; nchar++);
 
     new->len = nchar;
     new->next = *head;
