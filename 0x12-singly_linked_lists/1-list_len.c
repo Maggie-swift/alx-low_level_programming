fbd3acfa381fc7d3d3cea7674642372be802f9b6
Maggie-swift <buxomy8@gmail.com>
2023-03-30 00:57:13 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "lists.h"
+
 /**
  * list_len - returns then number of elements in a list.
  * @h: singly linked list.
--- 0x12-singly_linked_lists/2-main.c
+++ 0x12-singly_linked_lists/2-main.c
@@ -42,5 +42,5 @@ int main(void)
     add_node(&head, "William");
     add_node(&head, "Zee");
     print_list(head);
-    return (0)
+    return (0);
 }
