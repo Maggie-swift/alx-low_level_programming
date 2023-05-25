b8b4fc4dca633eb8994d388161b42c28e22e2a7a
Maggie-swift <buxomy8@gmail.com>
2023-05-25 06:52:55 -0700

    exit_s.c

--- simple_shell/exit_s.c
+++ simple_shell/exit_s.c
@@ -1,17 +1,17 @@
 #include "shell.h"
 
 /**
- *exit_c - Runs the exit builtin
- *@li : buffor for the line
- *@arg : args from the input
- *@env : enviroment
+ *exit_c - Runs exit builtin
+ *@li : buffer for the line
+ *@arg : args in the input
+ *@env : the enviroment created
  * Returns Void
  */
 
 void exit_c(char **arg, char *li, char **env)
 {
 	free(arg);
-	free(li);
+i	free(li);
 	(void)env;
 	exit(0);
 
