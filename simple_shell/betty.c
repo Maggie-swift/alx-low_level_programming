de9006d8bc8854576037358cb91e539e150afc13
Maggie-swift <buxomy8@gmail.com>
2023-05-26 15:31:27 -0700

    betty.c

---
+++ simple_shell/betty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *message = "Hello, Betty!";
+
+	printf("%s\n", message);
+
+	return (0);
+}
