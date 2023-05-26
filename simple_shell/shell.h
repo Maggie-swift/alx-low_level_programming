683301c7d7fa99b2f16c8bd3a052f7c829b0015f
Maggie-swift <buxomy8@gmail.com>
2023-05-26 15:07:34 -0700

    shell.h

---
+++ simple_shell/shell.h
@@ -0,0 +1,15 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#endif
